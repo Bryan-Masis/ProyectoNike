547979c4104d20b763daabcb4be63115438a9377
Bryan-Masis <98417167+Bryan-Masis@users.noreply.github.com>
2022-04-10 15:22:49 -0600

    Bryan Masis

---
+++ ProyectoNike-master/src/main/java/com/nikeT/Entity/Usuario.java
@@ -0,0 +1,71 @@
+package com.nikeT.Entity;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
+public class Usuario implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nombre;
+    private String pripellido;
+    private String segpellido;
+    private String email;
+    private String telefono;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPripellido() {
+        return pripellido;
+    }
+
+    public void setPripellido(String pripellido) {
+        this.pripellido = pripellido;
+    }
+
+    public String getSegpellido() {
+        return segpellido;
+    }
+
+    public void setSegpellido(String segpellido) {
+        this.segpellido = segpellido;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+}
